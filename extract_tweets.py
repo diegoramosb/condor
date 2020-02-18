dea544a84b23ed93d52d4f66d2373638fac28a70
Diego Ramos <da.ramos@uniandes.edu.co>
2020-02-17 20:29:01 -0500

    Changed storage to MongoDB

--- extract_tweets.py
+++ extract_tweets.py
@@ -1,8 +1,6 @@
 import tweepy
 import json
-import pymongo
-import firebase_admin as fb
-from firebase_admin import credentials, firestore
+from pymongo import MongoClient
 
 
 twitterKeys = {
@@ -17,14 +15,8 @@ auth.set_access_token(twitterKeys['accessTokenKey'], twitterKeys['accesTokenSecr
 
 api = tweepy.API(auth)
 
-accounts = ['@realDonaldTrump', '@petrogustavo']
-jsons = []
-
-cred = fb.credentials.Certificate("./proyecto2020-82afc-5d1e189a764c.json")
-fb.initialize_app(cred)
-db = firestore.client()
-
-def extractTweets():
+def extractTweetsApi(accounts):
+    jsons = []
     for id in accounts:
         for status in tweepy.Cursor(api.user_timeline, screen_name=id, tweet_mode="extended", count=5).items():
             jsonStr = json.dumps(status._json)
@@ -32,29 +24,28 @@ def extractTweets():
             jsons.append(parsed)
             if len(jsons) > 50:
                 break
+    return jsons
+
+def saveTweetsJsonFile(tweets):
     with open('tweets.json', 'w') as outfile:
-        json.dump(jsons, outfile)
+        json.dump(tweets, outfile)
 
-def searchTweets(word):
+def readTweetsJson():
+    with open('tweets.json', 'r') as file:
+        return json.load(file)
+
+def searchTweets(string, tweetArray):
     ans = []
-    with open('tweets.json', 'r') as file:
-        data = json.load(file)
-        for tweet in data:
-            if tweet['full_text'].find(word) != -1:
-                ans.append(tweet['full_text'])
-    return ans    
+    for tweet in tweetArray:
+        if tweet['full_text'].find(string) != -1:
+            ans.append(tweet)
+    return ans
 
-def saveTweets():
-    tweets = []
-    with open('tweets.json', 'r') as file:
-        data = json.load(file)
-        for tweet in data:
-            tweets.append(tweet)
+def saveTweetsMongo(tweets):
+    client = MongoClient('localhost', 27017)
+    db = client['proyecto2020']
+    collection = db['tweets']
     for t in tweets:
-        id = t['id']
-        print(t['place'])
-        # t['place'].pop('bounding_box')
-        # db.collection(u'tweets').document(str(id)).set(t)
-
-# saveTweets()
-print('done')
+        t['_id'] = t['id']
+        t.pop('id')
+        collection.insert_one(t)

762f32e56f60c4709a9b11c53059dbb8ed5d59da
Diego Ramos <da.ramos@uniandes.edu.co>
2020-02-24 19:28:13 -0500

    Organize methods, add documentation, fix duplicate tweets

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from extract_tweets import *
+from pymongo import MongoClient
 
 accounts = ['@ELTIEMPO', '@elespectador']
 
-tweets = extractTweetsApi(accounts)
-filteredTweets = searchTweets('Bogotá', tweets)
-saveTweetsMongo(filteredTweets)
+tweets = extractTweetsApi(accounts, 10)
+
+
+client = MongoClient('localhost', 27017)
+db = client['proyecto2020']
+collection = db['tweets']
+
+saveTweetsMongo(collection, tweets)
 print('done')

d1285bd94da03ca9d9f1b700ea04209e2028c940
Diego Ramos <da.ramos@uniandes.edu.co>
2020-02-24 19:28:13 -0500

    Organize methods, add documentation, fix duplicate tweets

--- extract_tweets.py
+++ extract_tweets.py
@@ -1,8 +1,8 @@
 import tweepy
 import json
-from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
 
-
+"""Twitter API keys"""
 twitterKeys = {
     'apiKey': 'M56Sn5yP02MGwzJCbn47PrL47',
     'apiSecret': 'NEO9ta8xgxqIWGKwW7IFjWdkDS6SMKDr2qZvbItJnPG3uEWIup',
@@ -10,42 +10,53 @@ twitterKeys = {
     'accesTokenSecret': '6MkOADmawkQqbP26douxoZ7cOMrpV4PgJfY6TLlzvQV1g'
 }
 
+"""Twitter API authentication"""
 auth = tweepy.OAuthHandler(twitterKeys['apiKey'], twitterKeys['apiSecret'])
 auth.set_access_token(twitterKeys['accessTokenKey'], twitterKeys['accesTokenSecret'])
-
 api = tweepy.API(auth)
 
-def extractTweetsApi(accounts):
+def extractTweetsApi(accounts, nTweets):
+    """
+    Extracts tweets from the selected accounts and returns a dict array containing them
+    :param accounts: string array with the account names. e.g. '@Uniandes'
+    :param nTweets: maximum number of tweets to extract from each account
+    :return: dict array with the tweets
+    """
     jsons = []
     for id in accounts:
-        for status in tweepy.Cursor(api.user_timeline, screen_name=id, tweet_mode="extended", count=5).items():
+        for status in tweepy.Cursor(api.user_timeline, screen_name=id, tweet_mode="extended").items(nTweets):
             jsonStr = json.dumps(status._json)
             parsed = json.loads(jsonStr)
             jsons.append(parsed)
-            if len(jsons) > 50:
-                break
     return jsons
 
-def saveTweetsJsonFile(tweets):
-    with open('tweets.json', 'w') as outfile:
-        json.dump(tweets, outfile)
-
-def readTweetsJson():
-    with open('tweets.json', 'r') as file:
-        return json.load(file)
-
 def searchTweets(string, tweetArray):
+    """
+    Searchs for a string in a tweet dict array
+    :param string: string to search
+    :param tweetArray: string of tweets in dict format
+    :return: the tweets with the searched string
+    """
     ans = []
     for tweet in tweetArray:
         if tweet['full_text'].find(string) != -1:
             ans.append(tweet)
     return ans
 
-def saveTweetsMongo(tweets):
-    client = MongoClient('localhost', 27017)
-    db = client['proyecto2020']
-    collection = db['tweets']
+def saveTweetsMongo(collection, tweets):
+    """
+    Saves the tweets in a MongoDB collection
+    :param collection: MongoDB collection object
+    :param tweets: tweets to save
+    """
+    cnt = 0
     for t in tweets:
         t['_id'] = t['id']
         t.pop('id')
-        collection.insert_one(t)
+        try:
+            collection.insert_one(t)
+            print("Added: {}".format(t['_id']))
+            cnt += 1
+        except DuplicateKeyError:
+            print("{} already in DB".format(t['_id']))
+    print("--- Added {} new tweets ---".format(cnt))

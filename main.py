a8e233d0bb0043b257293e655a6febfadd21622e
Diego Ramos <da.ramos@uniandes.edu.co>
2020-02-27 12:18:18 -0500

    Insert only tweets with relevant fields

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from extract_tweets import *
 from pymongo import MongoClient
 
-accounts = ['@ELTIEMPO', '@elespectador']
-
-tweets = extractTweetsApi(accounts, 10)
+accounts = ['@ELTIEMPO', '@elespectador', '@RevistaSemana']
 
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('172.24.99.115', 27017)
 db = client['proyecto2020']
 collection = db['tweets']
 
+tweets = extractTweetsApi(accounts, 10)
 saveTweetsMongo(collection, tweets)
-print('done')
+
+print("done")

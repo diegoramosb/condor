80554a30df12f7bafd0ddcf48aeb1ecaf42d8abf
Diego Ramos <da.ramos@uniandes.edu.co>
2020-03-02 16:08:28 -0500

    Style changes

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 from extract_tweets import *
-from pymongo import MongoClient
 from pprint import pprint
+from matplotlib.ticker import FuncFormatter
+import matplotlib.pyplot as plt
 
 accounts = ['@ELTIEMPO', '@elespectador', '@RevistaSemana']
-word = " Real madrid"
+word = "Real madrid"
 phrase = "\"Bogotá\""
 username= "@elespectador"
 
+# tweets = extractTweetsApi(accounts, 50)
+# saveTweetsMongo(collection, tweets, user)
 
 client = MongoClient('localhost', 27017)
 db = client['proyecto20202']
 collection = db['tweets']
 user =db['usuarios']
 
+ans = search_by_word(word)
+# search_by_phrases(collection,phrase, user)
+search_by_user(username)
+search_by_date(datetime(2020, 3, 1, 0, 0, 0), datetime(2020, 3, 2, 23, 59, 0))
 
-tweets = extractTweetsApi(accounts, 10)
+for a in ans:
+    pprint(a)
 
-saveTweetsMongo(collection, tweets, user)
 
 
-search_by_word(collection, word)
-search_by_phrases(collection,phrase, user)
-search_by_user(user,username, collection)
-search_by_date(collection, user)
 
 print("done")

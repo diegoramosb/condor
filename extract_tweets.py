89c0f9a724bfe5f08fa41210053549d8a0615e28
Mariana Moreno <mariana.anaya@hotmail.com>
2020-03-01 22:42:35 -0500

    searches

--- extract_tweets.py
+++ extract_tweets.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-
+from bson import ObjectId
 import tweepy
 import json
+from pprint import pprint
 from pymongo.errors import DuplicateKeyError
 
 """Twitter API keys"""
@@ -45,22 +46,67 @@ def searchTweets(string, tweetArray):
             ans.append(tweet)
     return ans
 
-def saveTweetsMongo(collection, tweets):
+def saveTweetsMongo(collection, tweets, user):
     """
     Saves the tweets in a MongoDB collection
     :param collection: MongoDB collection object
     :param tweets: tweets to save
     """
     cnt = 0
+
     for t in tweets:
         t['_id'] = t['id']
         date = datetime.strptime(t['created_at'], '%a %b %d %H:%M:%S %z %Y')
-        newTweet = {'_id':t['id'], 'text':t['full_text'], 'date':date}
+        newTweet = {'_id':t['id'], 'text':t['full_text'], 'date':date, 'userId':t['user']['id']}
         try:
             collection.insert_one(newTweet)
             print("Added: {}".format(t['_id']))
-            cnt += 1
+
         except DuplicateKeyError:
             print("{} already in DB".format(t['_id']))
+
+
+        if (user.find_one({"_id": t['user']['id']})) is None:
+                newuser = {'_id': t['user']['id'], 'name': t['user']['name'], 'screen_name': t['user']['screen_name']}
+                user.insert_one(newuser)
     print("--- Added {} new tweets ---".format(cnt))
 
+
+
+def search_by_word(tweets, word):
+
+    txt = tweets.find({ "$text": { "$search": word}})
+
+    for texto in txt:
+        pprint(texto)
+
+def search_by_phrases(tweets, ph, user):
+
+    txt = tweets.find({ "$text": { "$search": ph}})
+
+    for texto in txt:
+        us= user.find({'_id': texto['userId']})
+
+        pprint(texto)
+        pprint(us)
+
+
+
+def search_by_user (user, name, tweets):
+
+    us = user.find({"$text": {"$search": name}})
+
+    for inf in us:
+        tw = tweets.find({'userId': inf['_id']})
+        for t in tw:
+            pprint(t)
+
+
+def search_by_date (tweets,user):
+    startDate = datetime(2020, 2, 19, 0, 0, 0)
+    endDate = datetime(2020, 3, 2, 0, 0, 0)
+
+    txt = tweets.find({"date": {"$gte": startDate, "$lt": endDate}})
+
+    for texto in txt:
+        pprint(texto)

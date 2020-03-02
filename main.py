e8314f809a2ec6af759c1324e732337f518eb9bf
Mariana Moreno <mariana.anaya@hotmail.com>
2020-03-01 22:42:35 -0500

    searches

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from extract_tweets import *
 from pymongo import MongoClient
+from pprint import pprint
 
 accounts = ['@ELTIEMPO', '@elespectador', '@RevistaSemana']
+word = " Real madrid"
+phrase = "\"Bogotá\""
+username= "@elespectador"
 
 
-client = MongoClient('172.24.99.115', 27017)
-db = client['proyecto2020']
+client = MongoClient('localhost', 27017)
+db = client['proyecto20202']
 collection = db['tweets']
+user =db['usuarios']
+
 
 tweets = extractTweetsApi(accounts, 10)
-saveTweetsMongo(collection, tweets)
+
+saveTweetsMongo(collection, tweets, user)
+
+
+search_by_word(collection, word)
+search_by_phrases(collection,phrase, user)
+search_by_user(user,username, collection)
+search_by_date(collection, user)
 
 print("done")

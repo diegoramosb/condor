a564f7c1b4701d6d199f6e02aea822fa0367f6c1
Diego Ramos <da.ramos@uniandes.edu.co>
2020-03-02 19:37:30 -0500

    Frequency graph, organize files, add pycache to gitignore

--- extract_tweets.py
+++ extract_tweets.py
@@ -1,10 +1,5 @@
-from datetime import datetime
-from bson import ObjectId
 import tweepy
 import json
-from pymongo import MongoClient
-from pprint import pprint
-from pymongo.errors import DuplicateKeyError
 
 """Twitter API keys"""
 twitterKeys = {
@@ -19,12 +14,7 @@ auth = tweepy.OAuthHandler(twitterKeys['apiKey'], twitterKeys['apiSecret'])
 auth.set_access_token(twitterKeys['accessTokenKey'], twitterKeys['accesTokenSecret'])
 api = tweepy.API(auth)
 
-"""MongoDB connection"""
-# client = MongoClient('172.24.99.115', 27017)
-client = MongoClient('localhost', 27017)
-db = client['proyecto2020']
-tweets = db['tweets']
-users = db['usuarios']
+
 
 
 def extractTweetsApi(accounts, nTweets):
@@ -42,6 +32,7 @@ def extractTweetsApi(accounts, nTweets):
             jsons.append(parsed)
     return jsons
 
+
 def searchTweets(string, tweetArray):
     """
     Searchs for a string in a tweet dict array
@@ -54,49 +45,3 @@ def searchTweets(string, tweetArray):
         if tweet['full_text'].find(string) != -1:
             ans.append(tweet)
     return ans
-
-def saveTweetsMongo(collection, tweets, user):
-    """
-    Saves the tweets in a MongoDB collection
-    :param collection: MongoDB collection object
-    :param tweets: tweets to save
-    """
-    cnt = 0
-
-    for t in tweets:
-        t['_id'] = t['id']
-        date = datetime.strptime(t['created_at'], '%a %b %d %H:%M:%S %z %Y')
-        newTweet = {'_id':t['id'], 'text':t['full_text'], 'date':date, 'userId':t['user']['id']}
-        try:
-            collection.insert_one(newTweet)
-            print("Added: {}".format(t['_id']))
-            cnt += 1
-
-        except DuplicateKeyError:
-            print("{} already in DB".format(t['_id']))
-
-        if (user.find_one({"_id": t['user']['id']})) is None:
-                newuser = {'_id': t['user']['id'], 'name': t['user']['name'], 'screen_name': t['user']['screen_name']}
-                user.insert_one(newuser)
-    print("--- Added {} new tweets ---".format(cnt))
-
-def search_by_word(word):
-    return list(tweets.find({"$text": {"$search": word}}))
-
-def search_by_phrases(ph, user):
-    txt = tweets.find({ "$text": { "$search": ph}})
-
-    for texto in txt:
-        pprint(texto)
-        cursor = user.find({'_id': texto['userId']})
-        for usr in cursor:
-            pprint(usr)
-
-def search_by_user (name):
-    us = users.find({"$text": {"$search": name}})
-    for inf in us:
-        return list(tweets.find({'userId': inf['_id']}))
-
-
-def search_by_date (startDate, endDate):
-    return list(tweets.find({"date": {"$gte": startDate, "$lt": endDate}}))
--- main.py
+++ main.py
@@ -1,30 +1,22 @@
 from extract_tweets import *
 from pprint import pprint
-from matplotlib.ticker import FuncFormatter
-import matplotlib.pyplot as plt
+from db import *
+from graphs import *
 
 accounts = ['@ELTIEMPO', '@elespectador', '@RevistaSemana']
-word = "Real madrid"
-phrase = "\"Bogotá\""
+word = "Real Madrid"
+string = '"Real Madrid"'
 username= "@elespectador"
 
 # tweets = extractTweetsApi(accounts, 50)
-# saveTweetsMongo(collection, tweets, user)
+# saveTweetsMongo(tweets)
 
-client = MongoClient('localhost', 27017)
-db = client['proyecto20202']
-collection = db['tweets']
-user =db['usuarios']
-
-ans = search_by_word(word)
-# search_by_phrases(collection,phrase, user)
-search_by_user(username)
-search_by_date(datetime(2020, 3, 1, 0, 0, 0), datetime(2020, 3, 2, 23, 59, 0))
-
-for a in ans:
-    pprint(a)
+# results = search_by_word(word)
+print(search_by_string(string))
+# search_by_user(username)
+# search_by_date(datetime(2020, 3, 1, 0, 0, 0), datetime(2020, 3, 2, 23, 59, 0))
 
 
-
+# showWordFrequency(word)
 
 print("done")

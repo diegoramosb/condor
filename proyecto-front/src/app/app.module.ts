4a9e374cd04bc5ddf2f3f96ff7906a1a74dfd231
Mariana Moreno <mariana.anaya@hotmail.com>
2020-03-17 19:00:31 -0500

    avances graficas

--- proyecto-front/src/app/app.module.ts
+++ proyecto-front/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BubbleChartComponent } from './bubble-chart/bubble-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BubbleChartComponent
   ],
   imports: [
     BrowserModule,

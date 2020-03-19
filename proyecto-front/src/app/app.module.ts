b23f5e0873df2e3bc5460120ea9caec7846e98e7
Diego Ramos <da.ramos@uniandes.edu.co>
2020-03-19 16:12:38 -0500

    Bubble front

--- proyecto-front/src/app/app.module.ts
+++ proyecto-front/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ChartsModule } from 'ng2-charts';
+import { HttpClientModule } from '@angular/common/http' 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { MyBarChartComponent } from './my-bar-chart/my-bar-chart.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     ChartsModule
 

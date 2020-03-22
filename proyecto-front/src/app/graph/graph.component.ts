f4a791769100e5ecd9b4c41fc49a3bb2d1c209b8
Diego Ramos <da.ramos@uniandes.edu.co>
2020-03-21 20:03:55 -0500

    Gráficas front

---
+++ proyecto-front/src/app/graph/graph.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-graph',
+  templateUrl: './graph.component.html',
+  styleUrls: ['../app.component.css']
+})
+export class GraphComponent implements OnInit {
+
+  public scatterChartOptions: ChartOptions = {
+    responsive: true,
+  };
+
+  public scatterChartData: ChartDataSets[] = [
+    {
+      data: [
+        { x: 1, y: 1 },
+        { x: 2, y: 3 },
+        { x: 3, y: -2 },
+        { x: 4, y: 4 },
+        { x: 5, y: -3, r: 20 },
+      ],
+      label: 'Series A',
+      pointRadius: 10,
+    },
+  ];
+  public scatterChartType: ChartType = 'scatter';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

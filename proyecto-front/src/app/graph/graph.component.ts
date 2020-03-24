bf3e0af23ca527c55a04315709c272bc71694d63
Diego Ramos <da.ramos@uniandes.edu.co>
2020-03-24 12:26:28 -0500

    Dashboard

--- proyecto-front/src/app/graph/graph.component.ts
+++ proyecto-front/src/app/graph/graph.component.ts
@@ -20,7 +20,6 @@ export class GraphComponent implements OnInit {
         { x: 2, y: 3 },
         { x: 3, y: -2 },
         { x: 4, y: 4 },
-        { x: 5, y: -3, r: 20 },
       ],
       label: 'Series A',
       pointRadius: 10,

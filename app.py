482d969aa8e16eb4712c09f31e3241517beb2efc
Diego Ramos <da.ramos@uniandes.edu.co>
2020-03-28 17:07:20 -0500

    CORS, grafo

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask, jsonify
 from graphs import graph
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(graph)
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return jsonify({"text":"hello world"})
 
 @app.route("/home", methods=["GET"])
 def index():
@@ -15,4 +17,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=9090, debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=9090, debug=True, threaded=True)
--- proyecto-front/src/app/graph/graph.component.ts
+++ proyecto-front/src/app/graph/graph.component.ts
@@ -11,21 +11,65 @@ export class GraphComponent implements OnInit {
 
   public scatterChartOptions: ChartOptions = {
     responsive: true,
+    scales: {
+      xAxes: [{
+        ticks: {
+          min: 0,
+          max: 50,
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 10,
+        }
+      }]
+    }
   };
 
   public scatterChartData: ChartDataSets[] = [
     {
       data: [
-        { x: 1, y: 1 },
-        { x: 2, y: 3 },
-        { x: 3, y: -2 },
-        { x: 4, y: 4 },
+        { x: 10, y: 5, r: 10 },
       ],
-      label: 'Series A',
-      pointRadius: 10,
+      label: 'Cuenta1',
+      backgroundColor: "rgba(29, 161, 243, 1)",
+      type: 'bubble'
+    },
+    {
+      data: [
+        { x: 20, y: 3, r: 10 },
+      ],
+      label: 'Cuenta2',
+      backgroundColor: "rgba(25, 207, 134, 1)",
+      type: 'bubble'
+    },
+    {
+      data: [
+        { x: 30, y: 7, r: 10 },
+      ],
+      label: 'Cuenta3',
+      backgroundColor: "rgba(232, 28, 79, 1)",
+      type: 'bubble'
+    },
+    {
+      data: [{x: 10, y: 5}, {x: 20, y: 3}],
+      label: 'Palabra1',
+      type: 'line',
+      fill: false,
+      borderColor: "#E9F50F",
+      pointRadius: 0
+    },
+    {
+      data: [{x: 20, y: 3}, {x: 30, y: 7}],
+      label: 'Palabra2',
+      type: 'line',
+      fill: false,
+      borderColor: "#F50FDE",
+      pointRadius: 0
     },
   ];
-  public scatterChartType: ChartType = 'scatter';
+  public scatterChartType: ChartType = 'bubble';
 
   constructor() { }
 

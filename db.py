af387c4e3a5cf97bf39d615af8869b117ddce7e8
Diego Ramos <da.ramos@uniandes.edu.co>
2020-05-06 20:48:33 -0500

    Enable MongoDB authentication

--- db.py
+++ db.py
@@ -9,10 +9,9 @@ from utils import isAccount
 
 """MongoDB connection"""
 
-#client = MongoClient('172.24.99.115', 27017)
-
-client = MongoClient('172.24.99.115', 27017)
-# client = MongoClient('localhost', 27017)
+username = 'administrador'
+password = '&Si93JSSWh%udMqV1rtu'
+client = MongoClient('mongodb://{}:{}@172.24.99.115'.format(username, password))
 db = client['proyecto20203']
 
 tweetsCollection = db['tweets']

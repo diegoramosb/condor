b216044de81ab889ee6056754bbc1d5bf580541f
Diego Ramos <da.ramos@uniandes.edu.co>
2020-05-11 19:49:09 -0500

    Add follow accounts autocomplete

--- proyecto-front/src/app/settings/settings.component.ts
+++ proyecto-front/src/app/settings/settings.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { ApiService } from '../api.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith, debounce, debounceTime, switchMap } from 'rxjs/operators';
 
-export interface DialogData{
-  accounts: [];
+export interface DialogData {
+  accounts: User[];
   removedAccounts: number[];
   newAccounts: string[];
+}
+
+export interface User {
+  _id: number;
+  screen_name: string;
+  name: string;
+  profile_image: string;
 }
 
 @Component({
@@ -15,42 +25,59 @@ export interface DialogData{
 })
 export class SettingsComponent implements OnInit {
 
-  constructor(private apiService: ApiService, 
-    public dialogRef: MatDialogRef<SettingsComponent>, 
+  usersForm: FormGroup;
+
+  filteredUsers: User[];
+
+  constructor(private apiService: ApiService,
+    private formBuilder: FormBuilder,
+    public dialogRef: MatDialogRef<SettingsComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
   ngOnInit(): void {
-    // this.data.accounts.forEach(account => {
-    //   this.accountsStr.push(account['screen_name']);
-    // });
+    this.usersForm = this.formBuilder.group({ userInput: null });
+
+    this.usersForm
+      .get('userInput')
+      .valueChanges
+      .pipe(
+        switchMap(value => this.apiService.searchNewUser(value)
+        )
+      )
+      .subscribe((users: User[]) => this.filteredUsers = users);
   }
 
-  addAccount(account: string) {
-    var contains = false;
-    this.data.accounts.forEach(acc => {
-      if(acc['screen_name'] == account) {
-        contains = true;
+  addAccount(account: User) {
+    var contains1 = false;
+    var contains2 = false;
+    this.data.newAccounts.forEach(acc => {
+      if (acc == account['screen_name']) {
+        contains1 = true;
       }
     });
-    if(!contains) {
-      //getAccountAPI
-      this.data.newAccounts.push(account);
+    this.data.accounts.forEach(acc => {
+      if (acc["_id"] == account["_id"]) {
+        contains2 = true;
+      }
+    });
+    if ((contains1 && contains2) == false) {
+      this.data.newAccounts.push(account['screen_name']);
+      this.data.accounts.push(account);
     }
   }
 
   removeAccount(account: string) {
-    for(var i = 0; i < this.data.accounts.length; i++) {
-      if(this.data.accounts[i]['screen_name'] == account) {
+    for (var i = 0; i < this.data.accounts.length; i++) {
+      if (this.data.accounts[i]['screen_name'] == account) {
         this.data.removedAccounts.push(this.data.accounts[i]['_id']);
         this.data.accounts.splice(i, 1);
-        console.log(account)
       }
     }
   }
 
-  // onClickSave() {
-  //   this.removedAccounts;
-  // }
+  displayFn(user: User): string {
+    return user && user.name ? user.name : '';
+  }
 
   onNoClick(): void {
     this.dialogRef.close();

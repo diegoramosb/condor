5ab447f0d0862b52f641b8fdb20b482f413b8264
Diego Ramos <da.ramos@uniandes.edu.co>
2020-05-27 18:38:14 -0500

    Basic graph done

--- proyecto-front/src/app/graph/graph.component.ts
+++ proyecto-front/src/app/graph/graph.component.ts
@@ -1,7 +1,8 @@
 import { Component, ElementRef, Input, OnChanges, ViewChild, ViewEncapsulation, AfterViewInit } from '@angular/core';
 import * as d3 from 'd3';
-import { path } from 'd3';
+import { path, select } from 'd3';
 import { group } from '@angular/animations';
+import { stripGeneratedFileSuffix } from '@angular/compiler/src/aot/util';
 
 export interface GraphData {
   name: string;
@@ -22,9 +23,9 @@ export class GraphComponent implements OnChanges, AfterViewInit {
   @ViewChild('chart')
   private chartContainer: ElementRef;
 
-  height = 500;
+  height = (window.innerHeight * 70) / 100;
 
-  width = 500;
+  width = (window.innerWidth * 60) / 100;
 
   constructor() { }
 
@@ -52,9 +53,8 @@ export class GraphComponent implements OnChanges, AfterViewInit {
       .append('svg')
       .attr("height", this.height)
       .attr("width", this.width)
-      .style('background-color', 'yellow')
 
-    var nodes = this.calculateNodes(this.calculateWordRadius(), this.calculateAccountRadius())
+    var nodes = this.calculateNodes()
     var words = nodes[0];
     var accounts = nodes[1];
 
@@ -62,87 +62,104 @@ export class GraphComponent implements OnChanges, AfterViewInit {
       .attr("transform", `translate(${this.width / 2}, ${this.height / 2})`)
 
     accounts.forEach(account => {
-      group.append('circle')
-        .attr('cx', account['x'])
-        .attr('cy', account['y'])
-        .attr('r', 2)
-        .attr('fill', 'black')
-
       group.append('text')
         .attr('x', account['label']['x'])
         .attr('y', account['label']['y'])
-        .attr('text-anchor', 'start')
+        .attr('text-anchor', account['label']['anchor'])
         .attr('transform', `rotate(${account['label']['rot']} ${account['label']['x']} ${account['label']['y']})`)
         .text(account['label']['text'])
         .style('font-size', '1em')
     });
 
     words.forEach(word => {
-      group.append('circle')
-        .attr('cx', word['x'])
-        .attr('cy', word['y'])
-        .attr('r', 2)
-        .attr('fill', 'black')
-
       group.append('text')
+        .attr('class', 'word-' + word['label']['text'])
         .attr('x', word['label']['x'])
         .attr('y', word['label']['y'])
-        .attr('text-anchor', 'start')
+        .attr('text-anchor', word['label']['anchor'])
         .attr('transform', `rotate(${word['label']['rot']}, ${word['label']['x']}, ${word['label']['y']})`)
         .text(word['label']['text'])
         .style('font-size', '1em')
     })
 
+    var quadraticCurve = (x1, y1, x2, y2) => {
+      var path = d3.path();
+      path.moveTo(x1, y1);
+      path.quadraticCurveTo(0, 0, x2, y2);
+      return path.toString();
+    }
 
-    // var bezierCurve = (x1, y1, cp1x, cp1y, cp2x, cp2y, x2, y2) => {
-    //   var path = d3.path();
-    //   path.moveTo(x1, y1);
-    //   path.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x2, y2);
-    //   return path.toString();
-    // }
+    for (var i = 0; i < words.length; i++) {
+      var x1 = words[i]['x'];
+      var y1 = words[i]['y'];
+
+      for (var j = 0; j < words.length; j++) {
+        var x2 = words[j]['x'];
+        var y2 = words[j]['y'];
+        if (i != j && words[i]['label']['text'] == words[j]['label']['text']) {
+          group.append('path')
+            .attr('class', "word-" + words[i]['label']['text'])
+            .attr('d', quadraticCurve(x1, y1, x2, y2))
+            .attr('stroke', 'black')
+            .attr('stroke-width', '0.2em')
+            .attr('fill', 'none')
+        }
+      }
+    }
+
+    svg.selectAll('path')
+      .on('mouseover', () => {
+        var c = d3.select(d3.event.currentTarget).attr('class');
+        var selection = d3.selectAll("." + c.toString());
+        selection.filter(d3.matcher('text')).style('fill', 'blue');
+        selection.filter(d3.matcher('path')).attr('stroke', 'blue');
+      })
+      .on('mouseout', () => {
+        var c = d3.select(d3.event.currentTarget).attr('class');
+        var selection = d3.selectAll("." + c.toString());
+        selection.filter(d3.matcher('text')).style('fill', 'black');
+        selection.filter(d3.matcher('path')).attr('stroke', 'black');
+      })
+    
+    svg.selectAll('text')
+    .on('mouseover', () => {
+      var c = d3.select(d3.event.currentTarget).attr('class');
+      if(c != null) {
+        var selection = d3.selectAll("." + c.toString());
+        selection.filter(d3.matcher('text')).style('fill', 'blue');
+        selection.filter(d3.matcher('path')).attr('stroke', 'blue');
+      }
+    })
+    .on('mouseout', () => {
+      var c = d3.select(d3.event.currentTarget).attr('class');
+      if(c != null) {
+        var selection = d3.selectAll("." + c.toString());
+        selection.filter(d3.matcher('text')).style('fill', 'black');
+        selection.filter(d3.matcher('path')).attr('stroke', 'black');
+      }
+    })
 
     // svg.append('path').attr('d', bezierCurve(100, 100, 100, 200, 200, 100, 200, 200)).attr('stroke', 'black').attr('fill', 'none');
-
-    // var pathGenerator = (x1, y1, x2, y2, x3, y3) => {
-    //   var path = d3.path()
-    //   path.moveTo(x1, y1)
-    //   path.lineTo(x2, y2)
-    //   path.lineTo(x3, y3)
-    //   path.closePath()
-    //   return path.toString()
-    // }
-    // svg.append('path').attr('d', pathGenerator(25, 25, 75, 25, 200, 200))
-
-    // var lineGenerator = d3.line<any>()
-    // .x(function(d) {
-    //   return d[0]
-    // })
-    // .y(function (d) {
-    //   return d[1]
-    // })
-
-    // group.append("line")
-    //   .attr("x1", 200)
-    //   .attr("y1", 200)
-    //   .attr("x2", 300)
-    //   .attr("y2", 200)
-    //   .style("stroke", "rgb(255,0,0)")
-    //   .style("stroke-width", 2);
   }
 
   calculateWordRadius() {
     return 150;
   }
 
-  calculateAccountRadius() {
-    return this.calculateWordRadius() + 30;
-    //encontrar palabra más larga y sumar x*número de letras de la palabra
+  calculateAccountRadius(words) {
+    var longestWord = ""
+    words.forEach(word => {
+      if (word.length > longestWord.length) {
+        longestWord = word;
+      }
+    });
+    return this.calculateWordRadius() + longestWord.length * 15;
   }
 
   calculateRotation(x, y) {
     var textRotation = 0;
-    if(x > 0) {
-      if(y >= 0) {
+    if (x > 0) {
+      if (y >= 0) {
         textRotation = Math.atan2(y, x);
       }
       else {
@@ -150,62 +167,79 @@ export class GraphComponent implements OnChanges, AfterViewInit {
       }
     }
     else {
-      if(y > 0) {
+      if (y > 0) {
         textRotation = Math.atan2(y, x) - Math.PI;
       }
       else {
         textRotation = Math.atan2(y, x) - Math.PI;
       }
     }
-    return textRotation * (360/(2*Math.PI));
+    return textRotation * (360 / (2 * Math.PI));
   }
 
-  calculateNodes(wordRadius: number, accountRadius: number) {
+  calculateAnchor(x, y) {
+    if (x > 0) {
+      if (y >= 0) {
+        return "start"
+      }
+      else {
+        return "end"
+      }
+    }
+    else {
+      if (y > 0) {
+        return "end"
+      }
+      else {
+        return "end"
+      }
+    }
+  }
+
+  calculateNodes() {
     var words = [];
     var accounts = [];
     var accountScale = d3.scaleLinear()
       .domain([0, this.graphData.length])
       .range([0, 2 * Math.PI]);
 
-    var rotation = (accountScale(1) - accountScale(0))/2;
-    for (var i = 0; i < this.graphData.length; i++) {
-      var theta = accountScale(i) + rotation;
-      var x = (accountRadius + 5) * Math.sin(theta);
-      var y = (accountRadius + 5) * Math.cos(theta);
-      
-      var account = this.graphData[i];
-      accounts.push(
-        {
-          'label': {
-            'text': account.name,
-            'x': x,
-            'y': y,
-            'rot': this.calculateRotation(x, y)
-          },
-          'x': accountRadius * Math.sin(theta),
-          'y': accountRadius * Math.cos(theta)
-        }
-      );   
-      
-      if(account.words.length > 1) {
-        var start = accountScale(i);
-        var end = accountScale((i+1) % this.graphData.length) != 0 ? accountScale((i+1) % this.graphData.length) : 2 * Math.PI;
-        var mid = (end - start)/2;
-        var trueStart = start + 1.5*(mid/account.words.length);
-        var trueEnd = end - 1.5*(mid/account.words.length);
-        var increment = (trueEnd - trueStart)/(account.words.length - 1);
+    var rotation = (accountScale(1) - accountScale(0)) / 2;
+    var wordRadius = this.calculateWordRadius();
+    var allWords = [];
+    this.graphData.forEach(account => {
+      account.words.forEach(word => {
+        allWords.push(word);
+      })
+    });
+    var accountRadius = this.calculateAccountRadius(allWords);
 
+    for (var i = 0; i < this.graphData.length; i++) {
+      var account = this.graphData[i];
+
+      var theta = accountScale(i) + rotation;
+      var xAccount = (accountRadius + 5) * Math.sin(theta);
+      var yAccount = (accountRadius + 5) * Math.cos(theta);
+
+      var start = accountScale(i);
+      var end = accountScale((i + 1) % this.graphData.length) != 0 ? accountScale((i + 1) % this.graphData.length) : 2 * Math.PI;
+      var mid = (end - start) / 2;
+      var trueStart = start + 1.8 * (mid / account.words.length);
+      var trueEnd = end - 1.8 * (mid / account.words.length);
+      var increment = (trueEnd - trueStart) / (account.words.length - 1);
+
+      if (account.words.length > 1) {
         for (var j = 0; j < account.words.length; j++) {
           var alpha = trueStart + j * increment;
-          var x = (wordRadius + 5) * Math.sin(alpha);
-          var y = (wordRadius + 5) * Math.cos(alpha);
+          var xWord = (wordRadius + 5) * Math.sin(alpha);
+          var yWord = (wordRadius + 5) * Math.cos(alpha);
           words.push(
             {
               'label': {
                 'text': account.words[j],
-                'x': x,
-                'y': y,
-                'rot': this.calculateRotation(x, y)
+                'x': xWord,
+                'y': yWord,
+                'rot': this.calculateRotation(xWord, yWord),
+                'anchor': this.calculateAnchor(xWord, yWord)
               },
               'x': wordRadius * Math.sin(alpha),
               'y': wordRadius * Math.cos(alpha)
@@ -213,19 +247,37 @@ export class GraphComponent implements OnChanges, AfterViewInit {
           );
         }
       }
-      else if(account.words.length == 1) {
+      else if (account.words.length == 1) {
+        var xWord = (wordRadius + 5) * Math.sin(theta);
+        var yWord = (wordRadius + 5) * Math.cos(theta);
         words.push(
           {
             'label': {
               'text': account.words[0],
-              'x': (wordRadius + 5) * Math.sin(theta),
-              'y': (wordRadius + 5) * Math.cos(theta)
+              'x': xWord,
+              'y': yWord,
+              'rot': this.calculateRotation(xWord, yWord),
+              'anchor': this.calculateAnchor(xWord, yWord)
             },
             'x': wordRadius * Math.sin(theta),
             'y': wordRadius * Math.cos(theta)
           }
         );
       }
+
+      accounts.push(
+        {
+          'label': {
+            'text': account.name,
+            'x': xAccount,
+            'y': yAccount,
+            'rot': this.calculateRotation(xAccount, yAccount),
+            'anchor': this.calculateAnchor(xAccount, yAccount)
+          },
+          'x': accountRadius * Math.sin(theta),
+          'y': accountRadius * Math.cos(theta)
+        }
+      );
     }
 
     return [words, accounts];

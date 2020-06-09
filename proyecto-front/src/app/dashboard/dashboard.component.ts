15f019beea22a16eda1e9c7641e3aa81577048ee
Diego Ramos <da.ramos@uniandes.edu.co>
2020-06-09 17:26:49 -0500

    Fix tweets width, ufollow accounts

--- proyecto-front/src/app/dashboard/dashboard.component.ts
+++ proyecto-front/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ApiService } from '../api.service';
-import { SettingsComponent } from '../settings/settings.component';
+import { SettingsComponent, User } from '../settings/settings.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -39,16 +39,16 @@ export class DashboardComponent implements OnInit {
     localStorage.setItem('showing' + name, 'false');
     localStorage.removeItem(name2 + 'Filters');
     localStorage.removeItem(name2 + 'Data');
-    if(name == "Historic") {
+    if (name == "Historic") {
       this.showingHistoric = false;
     }
-    else if(name == "Bubble") {
+    else if (name == "Bubble") {
       this.showingBubble = false;
     }
-    else if(name == "Graph") {
+    else if (name == "Graph") {
       this.showingGraph = false;
     }
-    else if(name == "Freq") {
+    else if (name == "Freq") {
       this.showingFreq = false;
     }
   }
@@ -63,11 +63,11 @@ export class DashboardComponent implements OnInit {
 
       dialogRef.afterClosed().subscribe((result) => {
         if (result != undefined) {
-          result.removedAccounts.forEach((removedId: number) => {
-            this.apiService.unsubscribeFromAccount(removedId).subscribe(response => {
+          result.removedAccounts.forEach((account: User) => {
+            this.apiService.unsubscribeFromAccount(account['_id']).subscribe(() => {
+              this.removeAccountStorage(account['screen_name']);
               this.snackBar.open("Eliminados tweets de las cuentas seleccionadas", "Aceptar")
-            }
-            );
+            });
           });
           if (result.newAccounts.length > 0) {
             this.snackBar.open("Descargando tweets de las nuevas cuentas. Esto puede tardar unos segundos.", "Aceptar");
@@ -79,5 +79,30 @@ export class DashboardComponent implements OnInit {
         }
       });
     })
+  }
+
+  removeAccountStorage(name: string) {
+    var filters = [
+      { 'filter_name': 'historicFilters', 'filter': JSON.parse(localStorage.getItem('historicFilters')) },
+      { 'filter_name': 'bubbleFilters', 'filter': JSON.parse(localStorage.getItem('bubbleFilters')) },
+      { 'filter_name': 'graphFilters', 'filter': JSON.parse(localStorage.getItem('graphFilters')) },
+      { 'filter_name': 'freqFilters', 'filter': JSON.parse(localStorage.getItem('freqFilters')) },
+    ];
+
+    filters.forEach(filter => {
+      if (filter.filter != null) {
+        var newAccounts = []
+        filter.filter['accounts'].forEach((account: string) => {
+          if (name != account) {
+            newAccounts.push(account);
+          }
+        });
+        localStorage.setItem(filter['filter_name'], JSON.stringify({
+          'words': filter.filter['words'],
+          'accounts': newAccounts,
+          'date': filter.filter['date']
+        }));
+      }
+    });
   }
 }

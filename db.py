15cd952349fe5c0cf5a01a53d1bac55fc16fa419
Mariana Moreno <mariana.anaya@hotmail.com>
2020-06-10 19:09:46 -0500

    stopwords, indices

--- db.py
+++ db.py
@@ -208,64 +208,57 @@ def saveTweetsMongoOne(t, result):
     :param collection: MongoDB collection object
     :param tweets: tweets to save
     """
+
+
     success = False
     t['_id'] = t['id']
 
     date = datetime.strptime(t['created_at'], '%a %b %d %H:%M:%S %z %Y') - timedelta(hours=5)
+    newTweet = {}
 
-    newTweet ={}
-    if t['entities'] is None:
-        if t['entities']['media'] is None:
-            newTweet = {
-                '_id': t['id'],
-                'url': 'twitter.com/{}/status/{}'.format(t['user']['screen_name'], t['id']),
-                'text': t['text'],
-                'date': date,
-                'userId': t['user']['id'],
-                'retweet_count': [t['retweet_count']],
-                'favorite_count': [t['favorite_count']],
-                'request_times': [datetime.now()],
-                'polarity': result
-            }
-        else:
-            media = t['entities']['media'][0]['media_url']
-            newTweet = {
-                '_id': t['id'],
-                'url': 'twitter.com/{}/status/{}'.format(t['user']['screen_name'], t['id']),
-                'text': t['text'],
-                'media_link': media,
-                'date': date,
-                'userId': t['user']['id'],
-                'retweet_count': [t['retweet_count']],
-                'favorite_count': [t['favorite_count']],
-                'request_times': [datetime.now()],
-                'polarity': result
-            }
-    else:
-        newTweet = {
-            '_id': t['id'],
-            'url': 'twitter.com/{}/status/{}'.format(t['user']['screen_name'], t['id']),
-            'text': t['text'],
-            'date': date,
-            'userId': t['user']['id'],
-            'retweet_count': [t['retweet_count']],
-            'favorite_count': [t['favorite_count']],
-            'request_times': [datetime.now()],
-            'polarity': result
-        }
-    try:
-        tweetsCollection.insert_one(newTweet)
-        print("Added: {}".format(t['_id']))
-        # print(newTweet['text'])
-        print(newTweet)
-        success = True
-    except DuplicateKeyError:
-        print("{} already in DB".format(t['_id']))
-    if (usersCollection.find_one({"_id": t['user']['id']})) is None:
-        newuser = {'_id': t['user']['id'], 'name': t['user']['name'], 'screen_name': t['user']['screen_name'],
-                   'profile_image': t['user']['profile_image_url_https']}
-        usersCollection.insert_one(newuser)
-    return(success)
+
+    # try:
+    #     pprint(t['_id'])
+    #     newTweet = {
+    #         '_id': t['id'],
+    #         'url': 'twitter.com/{}/status/{}'.format(t['user']['screen_name'], t['id']),
+    #         'text': t['extended_tweet']["full_text"],
+    #         'date': date,
+    #         'userId': t['user']['id'],
+    #         'retweet_count': [t['retweet_count']],
+    #         'favorite_count': [t['favorite_count']],
+    #         'request_times': [datetime.now()],
+    #         'polarity': result
+    #     }
+    #     #pprint(t['_id'])
+    #
+    # except AttributeError:
+    #     newTweet = {
+    #         '_id': t['id'],
+    #         'url': 'twitter.com/{}/status/{}'.format(t['user']['screen_name'], t['id']),
+    #         'text': t['text'],
+    #         'date': date,
+    #         'userId': t['user']['id'],
+    #         'retweet_count': [t['retweet_count']],
+    #         'favorite_count': [t['favorite_count']],
+    #         'request_times': [datetime.now()],
+    #         'polarity': result
+    #
+    #     }
+    # finally:
+    #     try:
+    #         tweetsCollection.insert_one(newTweet)
+    #         print("Added: {}".format(t['_id']))
+    #         # print(newTweet['text'])
+    #         print(newTweet)
+    #         success = True
+    #     except DuplicateKeyError:
+    #         print("{} already in DB".format(t['_id']))
+    #     if (usersCollection.find_one({"_id": t['user']['id']})) is None:
+    #         newuser = {'_id': t['user']['id'], 'name': t['user']['name'], 'screen_name': t['user']['screen_name'],
+    #                    'profile_image': t['user']['profile_image_url_https']}
+    #         usersCollection.insert_one(newuser)
+    #     return (success)
 
 
 def updateTweets(tweets):

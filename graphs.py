d5a7d9092f8f37f5ed54c84d9c3dc88355ffa24a
Diego Ramos <da.ramos@uniandes.edu.co>
2020-06-11 17:14:58 -0500

    Delete unused method

--- graphs.py
+++ graphs.py
@@ -20,27 +20,6 @@ from extract_tweets import searchTweetById, extractTweetsApi
 graph = Blueprint('graphs', __name__)
 nltk.download('stopwords')
 
-def show_word_frequency(word):
-    userIds = []
-    usage = []
-    userNames = []
-    for tweet in search_by_keywords(word):
-        if tweet['userId'] not in userIds:
-            userIds.append(tweet['userId'])
-            usage.append(1)
-        else:
-            usage[userIds.index(tweet['userId'])] += 1
-    for userId in userIds:
-        usr = searchUserId(userId)
-        for username in usr:
-            userNames.append(username['name'])
-
-    n = np.arange(len(userNames))
-    plt.bar(n, usage)
-    plt.xticks(n, userNames)
-    plt.title('Número de menciones de "{}"'.format(word))
-    plt.show()
-
 def get_tweets_with_its_user(tweets):
 
     ans = []
